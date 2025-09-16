e16f07e2dd803fb0a12552d45d555b5eabe8161f
Bunnarin Chrun <chrunbunnarin857@gmail.com>
2025-09-16 08:26:13 +0700

    working payway

--- pb/pb_hooks/payway.pb.js
+++ pb/pb_hooks/payway.pb.js
@@ -3,10 +3,10 @@ routerAdd("POST", "/buy/{quantity}", (e) => {
     const amount = e.request.pathValue("quantity") * config.UNIT_PRICE()
     const req_time = Math.floor(Date.now() / 1000).toString()
     const email = e.auth.get('email')
-    const return_url = e.app.settings().meta.appUrl + '/payway/webhook'
+    const return_url = e.app.settings().meta.appURL + '/payway/webhook'
     const hashStr = req_time + config.PAYWAY_MERCHANT_ID() + req_time + amount + email + return_url
     const hash = $security.hs512(hashStr, config.PAYWAY_KEY())
-    return e.json(JSON.stringify({
+    return e.json(200, {
         hash,
         tran_id: req_time,
         amount,
@@ -14,9 +14,16 @@ routerAdd("POST", "/buy/{quantity}", (e) => {
         req_time,
         email,
         return_url,
-    }))
+    })
 }, $apis.requireAuth())
 
+
 routerAdd("POST", "/payway/webhook", (e) => {
-    
+    const config = require(`${__hooks}/config.js`)
+    const { amount, email } = e.request.body
+    const quantity = amount / config.UNIT_PRICE()
+    let user = $app.findFirstRecordByData("users", "email", email)
+    user.set('free_spots', user.get('free_spots') + quantity)
+    $app.save(user)
+    return e.json(200, { "message": "success" })
 })
