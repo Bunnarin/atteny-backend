3dbb2393ede6ee890cab2291bbf1904ab9031fb8
Bunnarin Chrun <chrunbunnarin857@gmail.com>
2025-10-12 22:43:54 +0700

    forgot to fix commonjs syntax

--- sheet-server/server.mjs
+++ sheet-server/server.mjs
@@ -1,6 +1,7 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config();
 
-const express = require('express');
+import express from 'express';
 import { GoogleSpreadsheet } from 'google-spreadsheet';
 import { OAuth2Client } from 'google-auth-library';
 
