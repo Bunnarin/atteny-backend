09a3c10e7681990918adaac7f1850fd21b4f9a3b
Bunnarin Chrun <chrunbunnarin857@gmail.com>
2025-10-16 09:20:30 +0700

    for now, manually entering formula

--- sheet-server/server.mjs
+++ sheet-server/server.mjs
@@ -32,7 +32,7 @@ app.post('/append', async (req, res) => {
         let sheet = doc.sheetsByTitle["attendance log"];
         if (!sheet) {
             sheet = await doc.addSheet({ title: "attendance log", headerValues: ['Date', 'Time', 'Name', 'Tag'] });
-            copy_dashboard(doc.spreadsheetId);
+            create_dashboard(doc);
         }
 
         try { 
@@ -66,23 +66,60 @@ app.post('/create', async (req, res) => {
 
 async function create_spreadsheet(workplace_name) {
     const doc = await GoogleSpreadsheet.createNewSpreadsheetDocument(oauthClient, { title: workplace_name + " (created by Atteny)" });
-    // copy template to new sheet
-    await doc.addSheet({ title: "attendance log", headerValues: ['Date', 'Time', 'Name', 'Tag'] });
-    copy_dashboard(doc.spreadsheetId);
+    // rename the default Sheet1
+    const defaultSheet = doc.sheetsByIndex[0];
+    await defaultSheet.updateProperties({ title: "attendance log" });
+    await defaultSheet.setHeaderRow(['Date', 'Time', 'Name', 'Tag']);
+    create_dashboard(doc);
     return doc;
 }
 
 // helper function to copy template to new sheet
-async function copy_dashboard(ssId) {
-    const original_token = oauthClient.credentials.refresh_token;
-    oauthClient.credentials.refresh_token = process.env.TEMPLATE_REFRESH_TOKEN;
-    const template_ss = new GoogleSpreadsheet(process.env.TEMPLATE_SS_ID, oauthClient);
-    await template_ss.loadInfo();
-    const dashboard_sheet = template_ss.sheetsByIndex[0];
-    dashboard_sheet.copyToSpreadsheet(ssId);
-    const tag_sheet = template_ss.sheetsByIndex[1];
-    tag_sheet.copyToSpreadsheet(ssId);
-    oauthClient.credentials.refresh_token = original_token;
+async function create_dashboard(doc) {
+    const dashboard_sheet = await doc.addSheet({ title: "attendance dashboard", headerValues: ['Please do not edit this sheet directly. Ctrl+A, Ctrl+C, Ctrl+shift+V to copy to another sheet']});
+    dashboard_sheet.addRows([
+        ['name', 'A', `=IFERROR(TRANSPOSE(SORT(UNIQUE(FILTER('attendance log'!A2:A999, 'attendance log'!A2:A999<>"")))),)`],
+        [
+            `=IFERROR(UNIQUE(FILTER('attendance log'!C2:C1000, 'attendance log'!C2:C1000<>"")),)`,
+            `=ARRAYFORMULA(BYROW(C3:AL101, LAMBDA(row, IF(COUNTIF(row, "A")=0, "", COUNTIF(row, "A")))))`,
+            `=ARRAYFORMULA(BYCOL(C2:AG2, 
+                  LAMBDA(date_col, 
+                    IF(date_col="",,
+                      BYROW(A3:A102, 
+                        LAMBDA(name_row, 
+                          IF(name_row="",, 
+                              IFERROR(TEXTJOIN(CHAR(10), TRUE, 
+                                FILTER(
+                                  'attendance log'!B2:B999, 
+                                  'attendance log'!A2:A999 = date_col, 
+                                  'attendance log'!C2:C999 = name_row
+                                )
+                              ), "A")
+                          )
+                        )
+                      )
+                    )
+                  )
+                ))`
+        ]
+    ]);
+    await dashboard_sheet.resize({ rowCount: 100, columnCount: 33 });
+    await dashboard_sheet.loadCells('A2:AG2');
+    // now we color the headerrow
+    for (let i = 0; i < 33; i++) {
+        const cell = dashboard_sheet.getCell(1, i);
+        // sht does not work and only give me blue color
+        cell.backgroundColor = {
+            red: 245,
+            green: 73,
+            blue: 39,
+        };
+        // cell.numberFormat = {
+        //     type: "DATE",
+        //     pattern: "mmmm d dddd"
+        // };
+    }
+    dashboard_sheet.saveUpdatedCells();
 }
 
 app.listen(3000, '127.0.0.1');
