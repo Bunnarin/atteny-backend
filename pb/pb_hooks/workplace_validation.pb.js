19a6739018339e3541da9aa32ea0fbfaaf26ca51
Bunnarin Chrun <chrunbunnarin857@gmail.com>
2025-10-26 15:12:57 +0700

    fixing the 429 error

--- pb/pb_hooks/workplace_validation.pb.js
+++ pb/pb_hooks/workplace_validation.pb.js
@@ -6,15 +6,11 @@ onRecordValidate((e) => {
     // paywall if the user doesnt have linked card
     const employees_changed = e.record.get('employees') != e.record.original().get('employees')
     if (employees_changed) {
-        // if no payment_methods, we check
-        const payment_method_num = $app.countRecords("payment_method", $dbx.hashExp({"user": employer.get('id')}));
-        if (!payment_method_num) {
-            const total_employees = $app.findRecordById("total_employees", employer.get('id')).get('value')
-            const free_spots = employer.get('max_employees') - total_employees
-            const diff = e.record.get('employees').length - e.record.original().get('employees').length
-            if (diff > free_spots)
-                throw new ApiError(403, "You have exceeded your free spots");
-        }
+        const total_employees = $app.findRecordById("total_employees", employer.get('id')).get('value')
+        const free_spots = employer.get('max_employees') - total_employees
+        const diff = e.record.get('employees').length - e.record.original().get('employees').length
+        if (diff > free_spots)
+            e.json(400) // this doesnt actually throws the error back to the /subscribe but it doesn throws an internal error
 
         // now we get_or_create employees
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
